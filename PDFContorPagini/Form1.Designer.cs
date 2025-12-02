99305a3f4749ba89da4809c010960f98382b111f
edvar <edvar@EDDY>
2025-12-02 18:20:15 +0200

    Add project files.

---
+++ PDFContorPagini/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace PDFContorPagini
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.txtFolderPath = new System.Windows.Forms.TextBox();
+            this.lblTotalPages = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelectFolder.Location = new System.Drawing.Point(21, 38);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(184, 42);
+            this.btnSelectFolder.TabIndex = 0;
+            this.btnSelectFolder.Text = "Selectează Folder";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // txtFolderPath
+            // 
+            this.txtFolderPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFolderPath.Location = new System.Drawing.Point(228, 38);
+            this.txtFolderPath.Name = "txtFolderPath";
+            this.txtFolderPath.ReadOnly = true;
+            this.txtFolderPath.Size = new System.Drawing.Size(500, 30);
+            this.txtFolderPath.TabIndex = 1;
+            // 
+            // lblTotalPages
+            // 
+            this.lblTotalPages.AutoSize = true;
+            this.lblTotalPages.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalPages.Location = new System.Drawing.Point(27, 143);
+            this.lblTotalPages.Name = "lblTotalPages";
+            this.lblTotalPages.Size = new System.Drawing.Size(135, 25);
+            this.lblTotalPages.TabIndex = 2;
+            this.lblTotalPages.Text = "Total pagini: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTotalPages);
+            this.Controls.Add(this.txtFolderPath);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Name = "Form1";
+            this.Text = "PDFContorPagini";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.TextBox txtFolderPath;
+        private System.Windows.Forms.Label lblTotalPages;
+    }
+}
+
---
+++ PDFContorPagini/Form1.cs
@@ -0,0 +1,82 @@
+﻿using PdfSharpCore.Pdf;
+using PdfSharpCore.Pdf.IO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDFContorPagini
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // You can add any initialization code here if needed.
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            // Folosește dialogul standard Windows pentru selectarea unui folder
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "Selectați directorul rădăcină care conține fișierele PDF.";
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                string rootFolderPath = fbd.SelectedPath;
+                txtFolderPath.Text = rootFolderPath; // Afișează calea selectată
+
+                try
+                {
+                    long totalPageCount = CountTotalPdfPages(rootFolderPath);
+                    lblTotalPages.Text = $"Total pagini: {totalPageCount}";
+                    MessageBox.Show($"Calcul finalizat. Total pagini: {totalPageCount}", "Succes");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"A apărut o eroare: {ex.Message}", "Eroare Majoră");
+                }
+            }
+        }
+
+        private long CountTotalPdfPages(string rootDirectoryPath)
+        {
+            long totalPages = 0;
+
+            // Caută toate fișierele .pdf recursiv (în subfoldere)
+            // Folosim SearchOption.AllDirectories
+            string[] pdfFiles = Directory.GetFiles(rootDirectoryPath, "*.pdf", SearchOption.AllDirectories);
+
+            foreach (string file in pdfFiles)
+            {
+                try
+                {
+                    // Deschide fișierul PDF folosind PDFsharp
+                    // Modul PdfDocumentOpenMode.ReadOnly este eficient
+                    using (PdfDocument document = PdfReader.Open(file, PdfDocumentOpenMode.ReadOnly))
+                    {
+                        totalPages += document.PageCount;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Ignoră fișierele corupte sau protejate cu parolă și raportează eroarea
+                    Console.WriteLine($"Eroare la citirea fișierului {Path.GetFileName(file)}: {ex.Message}");
+                    // Puteți adăuga o logare mai detaliată aici dacă doriți
+                }
+            }
+
+            return totalPages;
+        }
+    }
+}
